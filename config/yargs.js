4230de87aab093ba22db502b1668d64f649a874e
Victor Jaramillo E <victor87jaramillo@gmail.com>
2020-02-20 22:52:14 -0500

    Optimizacion de codigo

--- config/yargs.js
+++ config/yargs.js
@@ -1,44 +1,31 @@
-const optDescripcion = {
-    descripcion: {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea para crear'
-    }
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea para crear'
 };
+const completar = {
+    alias: 'c',
+    default: true,
+    desc: 'Marcacmo true a la tarea por hacer'
+}
 
 const argv = require('yargs')
     .command(
         'crear',
         'Permite crear un objeto', {
-            descripcion: {
-                demand: true,
-                alias: 'd',
-                desc: 'Descripcion de la tarea para crear'
-            }
+            descripcion
         }
     )
     .command(
         'update',
         'Permite actualiza el estado completado de una tarea', {
-            descripcion: {
-                demand: true,
-                alias: 'd',
-                desc: 'Descripcion de la tarea para crear'
-            },
-            completar: {
-                alias: 'c',
-                default: true,
-                desc: 'Marcacmo true a la tarea por hacer'
-            }
+            descripcion,
+            completar
         }
     ).command(
         'borrar',
         'Borra un elemento mediante la descripcion', {
-            descripcion: {
-                demand: true,
-                desc: 'Descripcion del elemento a borrar',
-                alias: 'd'
-            }
+            descripcion
         }
     )
     .help()
